21a28e62286440cf579355dd4074c70d9d20a21e
Romain Lancelot <rlancelot@myges;fr>
2022-11-01 15:37:14 +0100

    ex46 finish

--- Cours 12 - Structures/ESGI_2_TP_12_LANCELOT_Romain/46.c
+++ Cours 12 - Structures/ESGI_2_TP_12_LANCELOT_Romain/46.c
@@ -2,45 +2,55 @@
 #include <stdlib.h>
 #include <math.h>
 
+typedef struct Vecteur2d Vecteur2d;
+
 struct Vecteur2d {
     double x;
     double y;
 };
 
 // fonction translation de vecteur :
-
-struct Vecteur2d translation(struct Vecteur2d vecteur, double x, double y) {
-    vecteur.x += x;
-    vecteur.y += y;
-    return vecteur;
-}
-
-// fonction rotation de vecteur :
-
-struct Vecteur2d rotation(struct Vecteur2d vecteur, double angle) {
-    double x = vecteur.x;
-    double y = vecteur.y;
-    vecteur.x = x * cos(angle) - y * sin(angle);
-    vecteur.y = x * sin(angle) + y * cos(angle);
+Vecteur2d translation(Vecteur2d vecteur, Vecteur2d vTranslation) {
+    vecteur.x += vTranslation.x;
+    vecteur.y += vTranslation.y;
     return vecteur;
 }
 
 // fonction agrandissement de vecteur :
-
-struct Vecteur2d agrandissement(struct Vecteur2d vecteur, double facteur) {
-    vecteur.x *= facteur;
-    vecteur.y *= facteur;
+Vecteur2d agrandissement(Vecteur2d vecteur, Vecteur2d vAgrandissement, double rapport) {
+    vecteur.x = rapport * (vecteur.x - vAgrandissement.x) + vAgrandissement.x;
+    vecteur.y = rapport * (vecteur.y - vAgrandissement.y) + vAgrandissement.y;
     return vecteur;
 }
 
+// fonction rotation de vecteur :
+Vecteur2d rotation(Vecteur2d vecteur, Vecteur2d vRotation, double angle) {
+    vecteur.x = (vecteur.x - vRotation.x) * cos(angle) - (vecteur.y - vRotation.y) * sin(angle) + vRotation.x;
+    vecteur.y = (vecteur.x - vRotation.x) * sin(angle) + (vecteur.y - vRotation.y) * cos(angle) + vRotation.y;
+    return vecteur;
+}
+
+
 int main() {
-    struct Vecteur2d vecteur = {1, 1};
-    
-    vecteur = translation(vecteur, 1, 1);
-    vecteur = rotation(vecteur, M_PI / 2);
-    vecteur = agrandissement(vecteur, 2);
-    
-    printf("Vecteur : (%g, %g)", vecteur.x, vecteur.y);
+    Vecteur2d vecteur = {0, 0};
+    printf("Vecteur2d : (%g, %g)\n", vecteur.x, vecteur.y);
+
+    Vecteur2d vTranslation = {1, 2};
+    printf("Translation par un Vecteur2d : (%g, %g)\n", vTranslation.x, vTranslation.y);
+    vTranslation = translation(vecteur, vTranslation);
+    printf("(%g, %g)\n", vTranslation.x, vTranslation.y);
+
+    Vecteur2d vAgrandissement = {1, 0};
+    double rapport = 0.5;
+    printf("Agrandissement de rapport %g et de centre un Vecteur2d : (%g, %g)\n", rapport, vAgrandissement.x, vAgrandissement.y);    
+    vAgrandissement = agrandissement(vecteur, vAgrandissement, rapport);
+    printf("(%g, %g)\n", vAgrandissement.x, vAgrandissement.y);
+
+    Vecteur2d vRotation = {0, 2};
+    double angle = 135;
+    printf("Rotation d'angle %g deg et de centre un Vecteur2d : (%g, %g)\n", angle, vRotation.x, vRotation.y);    
+    vRotation = rotation(vecteur, vRotation, angle);
+    printf("(%e, %g)\n", vRotation.x, vRotation.y);
     
     exit(EXIT_SUCCESS);
 }
