7d57d9ce24dda406863936df00ff4b17c16b26ae
Romain Lancelot <rlancelot@myges;fr>
2022-11-23 22:53:52 +0100

    wip ex with 2 makefile for point and triangle

---
+++ Cours 13 - Programmation modulaire/ESGI_2_TP_13_LANCELOT_Romain/50/point.h
@@ -0,0 +1,25 @@
+#ifndef DEF_HEADER_POINT
+#define DEF_HEADER_POINT
+/* Protection du module */
+
+/**
+    Documentation du module et auteurs
+*/
+
+/* Macros publiques */
+
+/* Types publiques du module */
+typedef struct Point Point;
+struct Point {
+    double x;
+    double y;
+};
+
+/* Variables publiques du module */
+extern Point Point_creer(double x, double y);
+
+/* Fonctionnalités publiques du module */
+extern void Point_afficher(const Point * p);
+extern double Point_distance(const Point * first, const Point * second);
+
+#endif
---
+++ Cours 13 - Programmation modulaire/ESGI_2_TP_13_LANCELOT_Romain/50/triangle.h
@@ -0,0 +1,26 @@
+#ifndef DEF_HEADER_TRIANGLE
+#define DEF_HEADER_TRIANGLE
+/* Protection du module */
+
+/**
+    Documentation du module et auteurs
+*/
+
+/* Macros publiques */
+
+/* Types publiques du module */
+typedef struct Triangle Triangle;
+struct Triangle {
+    Point first;
+    Point second;
+    Point third;
+};
+
+/* Variables publiques du module */
+extern Triangle Triangle_creer(Point first, Point second, Point third);
+
+/* Fonctionnalités publiques du module */
+extern void Triangle_afficher(const Triangle * triangle);
+extern double Triangle_perimetre(const Triangle * triangle);
+
+#endif
