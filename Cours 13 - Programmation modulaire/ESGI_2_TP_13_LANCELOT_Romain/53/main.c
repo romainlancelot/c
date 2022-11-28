d1ab821f6f6bf72e19f081b652a975c5bd5e63de
Romain <rlancelot@myges.fr>
2022-11-28 22:08:32 +0100

    create ex 53 files

---
+++ Cours 13 - Programmation modulaire/ESGI_2_TP_13_LANCELOT_Romain/53/main.c
@@ -0,0 +1,21 @@
+// Romain LANCELOT - 2ESGI A4
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node Node;
+struct Node {
+    char key;
+    int value;
+    Node * lower;
+    Node * upper;
+    Node * next;
+    Node * prev;
+};
+
+int main() {
+    
+
+    exit(EXIT_SUCCESS);
+}
